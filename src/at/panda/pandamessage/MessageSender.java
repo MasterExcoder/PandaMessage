0148aeb4529a9e2c0b7093271e96138b775f4e4c
Samuel Hammer <ham14447@spengergasse.at>
2014-03-20 13:57:38 +0100

    Added Constructor in MessageSender.java

--- src/at/panda/pandamessage/MessageSender.java
+++ src/at/panda/pandamessage/MessageSender.java
@@ -1,11 +1,26 @@
 package at.panda.pandamessage;
 
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+
+import android.content.Context;
+
 public class MessageSender extends Message implements Runnable {
 
+	Context context;
 	
-	
-	public MessageSender() {
-		
+	public MessageSender(Context context, InetAddress ip, int port, String content) {
+		this.context = context;
+		setIp(ip);
+		setPort(port);
+		try {
+			setSocket(new DatagramSocket(port));
+		} catch (SocketException e) {
+			e.printStackTrace();
+		}
+		setContent(content);
 	}
 
 	@Override
