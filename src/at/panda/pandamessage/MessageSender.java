e842c60a1a19dcb8aeb25f75fad9127d0b56b5eb
Samuel Hammer <ham14447@spengergasse.at>
2014-03-20 12:56:29 +0100

    Created class MessageSender

---
+++ src/at/panda/pandamessage/MessageSender.java
@@ -0,0 +1,16 @@
+package at.panda.pandamessage;
+
+public class MessageSender extends Message implements Runnable {
+
+	
+	
+	public MessageSender() {
+		
+	}
+
+	@Override
+	public void run() {
+		
+	}
+
+}
